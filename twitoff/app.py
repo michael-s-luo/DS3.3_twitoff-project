129ec3e03a13b0e60e9079fcc6efbda4de6900ed
michael-s-luo <michael.luo@outlook.com>
2023-05-15 17:07:15 -0700

    changed to production posgresql db (elephantsql), fixed padding

--- twitoff/app.py
+++ twitoff/app.py
@@ -5,6 +5,7 @@ Flask.
 """
 
 from flask import Flask, render_template, request
+from os import getenv
 from .models import DB, User, Tweet
 from .twitter import add_or_update_user
 from .predict import predict_user
@@ -14,16 +15,42 @@ def create_app():
     app = Flask(__name__)
 
     # DB configurations, connect database & app
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     DB.init_app(app)
 
     @app.route("/")
     def root():
+        """Root page by default will reset all tables and only have tweets
+        from nasa, nike, and npr for demo purposes.
+
+        TwitOff logo now routes to /home
+
+        This change was for demo purposes for people directed to the site from
+        my resume, LinkedIn, etc.
+        """
+
+        # Query users table for the home page
+        DB.drop_all()
+        DB.create_all()
+        add_or_update_user("nasa")
+        add_or_update_user("nike")
+        add_or_update_user("npr")
+        users = User.query.all()
+
+        return render_template(
+            "base.html", title="Welcome to Twitoff!", users=users
+        )
+
+    @app.route("/home")
+    def home():
         # Query users table for the home page
         users = User.query.all()
 
-        return render_template("base.html", title="Home", users=users)
+        return render_template(
+            "base.html", title="Select Users for Comparison", users=users
+        )
 
     @app.route("/reset")
     def reset():
